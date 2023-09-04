393535f3b40bd13832da3136cb4796801190d595
Sommod <Sommod@192.168.0.105>
2023-09-04 09:40:18 -0400

    Updated Select.cpp

--- Assignment 1/select.cpp
+++ Assignment 1/select.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
 
+int main() {
+    std::string temp = "";
+    int A[30] = {-1000};
+    bool complete = false;
+
+    for(int i = 0; complete != true, i < 30; i++) {
+        std::cout << "Enter a number (-1000 to stop): ";
+        std::cin >> A[i];
+    }
+
+    SelectionSort(A);
+
+    std::string display = "";
+
+    for(int i = 0; i < sizeof(A), A[i] != -1000; i++) {
+        display = display.append(A[i] + ",");
+    }
+
+    std::cout << display;
+
+    return 0;
+}
+
+void SelectionSort(int A[]) {
+    for(int i = 0; i < sizeof(A), A[i] != -1000; i++) {
+        int index = i;
+
+        for(int j = i + 1; j < sizeof(A), A[j] != -1000; j++) {
+            if(A[j] < A[i]) {
+                index = j;
+            }
+        }
+
+        if(index != i) {
+            int temp = A[i];
+            A[i] = A[index];
+            A[index] = temp;
+        }
+    }
+}
