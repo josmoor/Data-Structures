4020dfcd4de9bb3b061f409a4e2f32d4120615f4
Sommod <Sommod@192.168.0.105>
2023-09-07 19:03:23 -0400

    Added output to file method

--- Assignment 2/obscure.cpp
+++ Assignment 2/obscure.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
+#include <rectObj.h>
 using namespace std;
 
 // Functions
@@ -8,24 +9,35 @@ void generateRect(int);
 
 // Variables
 int numRect;
+Rectangle rects[];
 
 int main() {
     cout << "Enter number of rectangles: ";
     cin >> numRect;
 
     generateRect(numRect);
-
-    ofstream outFile;
-    outFile.open("rectData.txt");
-    outFile << numRect;
-    outFile.flush();
-    outFile.close();
+    writeData();
 
     return 0;
+}
+
+void writeData() {
+    ofstream outputFile;
+
+    outputFile.open("rectData.txt");
+
+    //TODO Loop
+
+    outputFile.flush();
+    outputFile.close();
 }
 
 void generateRect(int amount) {
     for(int i = 0; i < amount; i++) {
         bool isOverlap = true;
+
+        while(isOverlap) {
+            
+        }
     }
 }
