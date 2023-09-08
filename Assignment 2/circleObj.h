01f218ca8a01000a23cb9ad08db4e3e6e8f7f5dd
BuildTools <unconfigured@null.spigotmc.org>
2023-09-08 06:50:52 -0400

    Added object functions

--- Assignment 2/circleObj.h
+++ Assignment 2/circleObj.h
@@ -2,31 +2,21 @@
 #include <rectObj.h>
 
 class Circle {
+    Circle(float center[2], float radius) {
+        pos[0] = center[0];
+        pos[1] = center[1];
+        this->radius = radius;
+        dis = sqrt((center[0]^2 + center[1]^2));
+    }
+
     private:
         float pos[2];
         float radius;
+        float dis;  
 
     public:
-        bool isInCircle(float pos[2], bool top) {
-            if(top) { // If other circle is above
-                for(int i = 0; i < 180; i++) {
-                    float tempPos[2];
-                    tempPos[0] = this->pos[0] + (radius * sin(i));
-                    tempPos[1] = this->pos[1] + (radius * cos(i));
-
-                    if(tempPos[0] == pos[0] && tempPos[1] == pos[1])
-                        return true;
-                }
-            } else {
-                for(int i = 180; i < 360; i++) {
-                    float tempPos[2];
-                    tempPos[0] = this->pos[0] + (radius * sin(i));
-                    tempPos[1] = this->pos[1] +  (radius * cos(i));
-
-                    if(tempPos[0] == pos[0] && tempPos[1] == pos[1])
-                        return true;
-                }
-            }
+        bool isInCircle(float pos[2], float radius) {
+            //TODO: Check if given circle data is within this circle data
 
             return false;
         }
@@ -43,20 +33,18 @@ class Circle {
         float getRadius() { return radius; }
         float getX() { return pos[0]; }
         float getY() { return pos[1]; }
+        float getDistance() { return dis; }
 
         Rectangle getRect() {
-            Rectangle rect;
-
             //TODO: Get random point on circle.
             //Q2angle = (90 - angle) + 90
             //Q3angle = 180 + angle
             //Q4angle = 360 - angle
             //TODO: get points on circle
             //TODO: get random length (max of circle point)
-            //TODO: get obscure angle of origin
 
             //TODO: store in rect
 
-            return rect;
+            return new Rectangle(); //{Center}, {Position}, Length, Width, Radius (circle of all rects)
         }
 };
--- Assignment 2/rectObj.h
+++ Assignment 2/rectObj.h
@@ -1,11 +1,24 @@
 #include <iostream>
 
 class Rectangle {
+    Rectangle(float centerPoint[2], float startPoint[2], float length, float width, float radius) {
+        dis = centerPoint;
+        pos = startPoint;
+        height[0] = length;
+        height[1] = width;
+
+        calcAngle(radius);
+    }
+
     private:
         float dis[2]; // X/Y Position from origin (Center of Rectangle)
         float pos[2]; // X - Y
         float height[2]; // Length - Height
         float angle[2]; // Min - Max
+
+        void calcAngle(float radius) {
+            // TODO: Calculate the min/max angle
+        }
 
     public:
         void setDistance(float x, float y) {
@@ -22,4 +35,17 @@ class Rectangle {
             height[0] = l;
             height[1] = h;
         }
+        void setAngle(float min, float max) {
+            angle[0] = min;
+            angle[1] = max;
+        }
+
+        float getCenterX() { return dis[0]; }
+        float getCenterY() { return dis[1]; }
+        float getPointX() { return pos[0]; }
+        float getPointY() { return pos[1]; }
+        float getLength() { return height[0]; }
+        float getWidth() { return height[1]; }
+        float getMinAngleObscure() { return angle[0]; }
+        float getMaxAngleObscure() { return angle[1]; }
 };
