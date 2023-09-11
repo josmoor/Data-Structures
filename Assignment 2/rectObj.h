1735a70d3b42e535e52cc22aadedf18f8609f607
Sommod <jm0350@yahoo.com>
2023-09-11 11:17:01 -0400

    Finished Rectangle information data

--- Assignment 2/rectObj.h
+++ Assignment 2/rectObj.h
@@ -1,51 +1,42 @@
 #include <iostream>
+#include <math.h>
 
 class Rectangle {
-    Rectangle(float centerPoint[2], float startPoint[2], float length, float width, float radius) {
-        dis = centerPoint;
-        pos = startPoint;
-        height[0] = length;
-        height[1] = width;
+    Rectangle(float[2] centerPoint, float[8] points, float[2] dimensions, float radius) {
+        dis[0] = centerPoint[0];
+        dis[1] = centerPoint[1];
+        height[0] = dimensions[0];
+        height[1] = dimensions[1];
+
+        for(int i = 0; i < 8; i++)
+            pos[i] = point[i];
 
         calcAngle(radius);
     }
 
     private:
         float dis[2]; // X/Y Position from origin (Center of Rectangle)
-        float pos[2]; // X - Y
+        float pos[8]; // {xy,xy,xy,xy}
         float height[2]; // Length - Height
         float angle[2]; // Min - Max
 
         void calcAngle(float radius) {
-            // TODO: Calculate the min/max angle
+            int[] xIndex = {0,0};
+
+            for(int i = 2; i < 8; i += 2) {
+                if(pos[i] > pos[xIndex[1]])
+                    xIndex[1] = i;
+                if(pos[i] < pos[xIndex[0]])
+                    xIndex[0] = i;
+            }
+
+            // angle[0] = ARCtan(pos[xIndex[0] + 1] / pos[xIndex[0]]) - Min Angle
+            // angle[1] = ARCtan(pos[xIndex[1] + 1] / pos[xIndex[1]]) - Max angle
         }
 
     public:
-        void setDistance(float x, float y) {
-            dis[0] = x;
-            dis[1] = y;
-        }
-
-        void setPosition(float x, float y) {
-            pos[0] = x;
-            pos[1] = y;
-        }
-
-        void setHeight(float l, float h) {
-            height[0] = l;
-            height[1] = h;
-        }
-        void setAngle(float min, float max) {
-            angle[0] = min;
-            angle[1] = max;
-        }
-
-        float getCenterX() { return dis[0]; }
-        float getCenterY() { return dis[1]; }
-        float getPointX() { return pos[0]; }
-        float getPointY() { return pos[1]; }
-        float getLength() { return height[0]; }
-        float getWidth() { return height[1]; }
-        float getMinAngleObscure() { return angle[0]; }
-        float getMaxAngleObscure() { return angle[1]; }
+        float[] getCenter() { return new float[2] {dis[0], dis[1]}; }
+        float[] getPoint(int point) { return new float[2] {pos[point * 2], pos[(point * 2) + 1]}; }
+        float[] getDimensions() { return new float[2] {height[0], height[1]}; }
+        float[] getMinAngleObscure() { return new float[2] {angle[0], angle[1]}; }
 };
