ecb1f88554918abb560ab13e54b4ffc9e558bc0a
Moore <josmoor@iu.edu>
2023-09-11 16:04:54 -0400

    Added functions

--- Assignment 2/circleObj.h
+++ Assignment 2/circleObj.h
@@ -1,22 +1,42 @@
 #include <math.h>
 #include <rectObj.h>
+#include <stdlib.h>
+#include <time.h>
 
 class Circle {
     Circle(float center[2], float radius) {
         pos[0] = center[0];
         pos[1] = center[1];
         this->radius = radius;
-        dis = sqrt((center[0]^2 + center[1]^2));
     }
 
     private:
         float pos[2];
         float radius;
-        float dis;  
 
     public:
         bool isInCircle(float pos[2], float radius) {
-            //TODO: Check if given circle data is within this circle data
+            if(this->pos[1] > pos[1]) { // This circle on top
+                if(this->pos[0] > pos[0]) { // This circle right
+                    if(this->pos[0] - this->radius < pos[0] + radius &&
+                        this->pos[1] - this->radius < pos[1] + radius)
+                            return true;
+                } else { // Other circle right
+                    if(this->pos[0] + this-> radius < pos[0] - radius &&
+                        this->pos[1] - this->radius < pos[1] + radius)
+                            return true;
+                }
+            } else { // Other circle on top
+                if(this->pos[0] > pos[0]) { // This circle right
+                    if(this->pos[0] - this->radius < pos[0] + radius &&
+                        this->pos[1] + this->radius < pos[1] + radius)
+                            return true;
+                } else { // Other circle right
+                    if(this->pos[0] + this-> radius < pos[0] - radius &&
+                        this->pos[1] + this->radius < pos[1] + radius)
+                            return true;
+                }
+            }
 
             return false;
         }
@@ -33,9 +53,37 @@ class Circle {
         float getRadius() { return radius; }
         float getX() { return pos[0]; }
         float getY() { return pos[1]; }
-        float getDistance() { return dis; }
+
+        int getAngle() {
+            int ret = rand() % 179 + 1;
+
+            while(ret == 90)
+                ret = rand() % 179 + 1;
+
+            return ret;
+        }
 
         Rectangle getRect() {
+            srand(time(NULL));
+            int p1Angle = getAngle();
+            int p2Angle = getAngle();
+            bool[2] Q = {true, true};
+
+            if(p1Angle > 90)
+                Q[0] = false;
+            if(p2Angle > 90)
+                Q[1] = false;
+
+            if(Q[0] && Q[1]) {
+                if(Q[0]) { // Q1
+                   //TODO: Both points in Q1 
+                } else { // Q2
+                    //TODO: Both points in Q2
+                }
+            } else {
+
+            }
+
             //TODO: Get random point on circle.
             //Q2angle = (90 - angle) + 90
             //Q3angle = 180 + angle
@@ -45,6 +93,6 @@ class Circle {
 
             //TODO: store in rect
 
-            return new Rectangle(); //{Center}, {Position}, Length, Width, Radius (circle of all rects)
+            return new Rectangle(); //{float[2]}, {float[8]}
         }
 };
--- Assignment 2/rectObj.h
+++ Assignment 2/rectObj.h
@@ -1,25 +1,22 @@
 #include <iostream>
 #include <math.h>
+#DEFINE PI 3.1415926536
 
 class Rectangle {
-    Rectangle(float[2] centerPoint, float[8] points, float[2] dimensions, float radius) {
+    Rectangle(float[2] centerPoint, float[8] points) {
         dis[0] = centerPoint[0];
         dis[1] = centerPoint[1];
-        height[0] = dimensions[0];
-        height[1] = dimensions[1];
 
         for(int i = 0; i < 8; i++)
             pos[i] = point[i];
-
-        calcAngle(radius);
     }
 
     private:
         float dis[2]; // X/Y Position from origin (Center of Rectangle)
         float pos[8]; // {xy,xy,xy,xy}
-        float height[2]; // Length - Height
         float angle[2]; // Min - Max
 
+    public:
         void calcAngle(float radius) {
             int[] xIndex = {0,0};
 
@@ -30,13 +27,11 @@ class Rectangle {
                     xIndex[0] = i;
             }
 
-            // angle[0] = ARCtan(pos[xIndex[0] + 1] / pos[xIndex[0]]) - Min Angle
-            // angle[1] = ARCtan(pos[xIndex[1] + 1] / pos[xIndex[1]]) - Max angle
+            angle[0] = atan(pos[xIndex[0] + 1] / pos[xIndex[0]]) * 180 / PI;
+            angle[1] = atan(pos[xIndex[1] + 1] / pos[xIndex[1]]) * 180 / PI;
         }
 
-    public:
         float[] getCenter() { return new float[2] {dis[0], dis[1]}; }
         float[] getPoint(int point) { return new float[2] {pos[point * 2], pos[(point * 2) + 1]}; }
-        float[] getDimensions() { return new float[2] {height[0], height[1]}; }
         float[] getMinAngleObscure() { return new float[2] {angle[0], angle[1]}; }
 };
