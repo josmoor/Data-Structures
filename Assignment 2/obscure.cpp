1f0cf0da4c02c083417c25903d5480894960d8b1
Sommod <Sommod@192.168.0.105>
2023-09-13 20:58:08 -0400

    Updated Main class

--- Assignment 2/obscure.cpp
+++ Assignment 2/obscure.cpp
@@ -25,7 +25,7 @@ int main() {
     generateCircles(numRect);
 
     for(int i = 0; i < numRect; i++) {
-        rects.at(i) = * new Rectangle(circles[i].getX(), circles[i].getY(), circles[i].getRadius());
+        rects.at(i) = new Rectangle(circles[i].getX(), circles[i].getY(), circles[i].getRadius());
     }
 
     writeData();
@@ -78,7 +78,7 @@ void generateCircles(int amount) {
         bool isOverlap = true;
         float points[2] = {rand() % 10 + 1,rand() % 10 + 1};
 
-        circles.at(i) = * new Circle(points, rand() % 15 + 1);
+        circles.at(i) = new Circle(points, rand() % 15 + 1);
 
         while(isOverlap) {
             if(!circles.at(i).isInCircle({1.1,1.1}, 1))
