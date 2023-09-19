730192fc37d1bb1cb21341b084bf991a58337043
Sommod <Sommod@192.168.0.105>
2023-09-19 19:19:53 -0400

    push

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -70,6 +70,7 @@ Description:
 */
 void placeQueens(bool moveQueen) {
     if(end < Vars::size) { // If there is still a queen that can be moved (ie. 1 or 100 queen(s) left to move)
+        std::cout << "end:" << end << " moveQueen: " << (moveQueen ? "true" : "false") << " empty size: " << empty.size() << std::endl;
         if(moveQueen) { // No spot for current queen. Reset column to 1st, push queen back, move previous queen
             full.push_back(empty.top());
             empty.pop();
