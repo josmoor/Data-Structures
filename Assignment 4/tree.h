377e13b2b7e643ba7c1f2767c78535101c69fe95
josmoor <josmoor@iu.edu>
2023-09-27 14:50:44 -0900

    redo Tree Class

--- Assignment 4/tree.h
+++ Assignment 4/tree.h
@@ -3,52 +3,16 @@
 
 class Tree {
     private:
-        Tree *parent;
-        Tree *left;
-        Tree *right;
+        Tree left;
+        Tree right;
+        Tree parent;
         int value;
-        int height, depth;
 
     public:
-        Tree(Tree *parent, int value, int height, int depth) {
-            *this->parent = *parent;
+        Tree(int value, Tree parent) {
             this->value = value;
-            this->height = height;
-            this->depth = depth;
+            this->parent = parent;
         }
-        ~Tree() {
-            *parent = null;
-            *left = null;
-            *right = null;
-        }
-
-        void setParnet(Tree *parent) {
-            *this->parent = *parent;
-        }
-
-        void setLeft(Tree *left) {
-            *this->left = *left;
-        }
-
-        void setRight(Tree *right) {
-            *this->right = *right;
-        }
-
-        void setHeight(int value) {
-            height = value;
-        }
-
-        void setDepth(int value) {
-            depth = value;
-        }
-
-        int getValue() { return value; }
-        int getHeight() { return height; }
-        int getDepth() { return depth; }
-
-        Tree * getParnet() { return *parnet; }
-        Tree * getLeftChild() { return *left; }
-        Tree * getRightChild() { return *right; }
-}
+};
 
 #endif
