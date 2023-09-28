3c8277fa71cea98c55920f24bf03e0ed32fdf53b
josmoor <josmoor@iu.edu>
2023-09-27 17:42:22 -0900

    Completed assignment

--- Assignment 4/main.cpp
+++ Assignment 4/main.cpp
@@ -4,17 +4,28 @@
 
 // Functions
 void getInput();
-void place();
+Tree * place(Tree*, int);
+void storeElements();
 void display();
 
 // Variables
-Tree *first;
+Tree *first = nullptr;
 std::vector<int> values;
+std::vector<int> element;
 
 int main() {
 
     getInput();
-    place();
+    
+    int mid = values.size() % 2 == 0 ? values.size() / 2 + 1: values.size() / 2;
+    first = new Tree(values.at(mid));
+
+    for(int i = mid - 1; i >= 0; i--)
+        place(first, values.at(i));
+
+    for(int i = mid + 1; i < values.size(); i++)
+        place(first, values.at(i));
+
     display();
 
     return 0;
@@ -35,21 +46,31 @@ void getInput() {
         values.push_back(input);
     }
 
-    for(int i = 0; i < values.size(); i++) {
-        for(int j = i + 1; j < values.size(); j++) {
-            if(values.at(i) < values.at(j)) {
-                int tmp = values.at(i);
-                values.at(i) = values.at(j);
-                values.at(j) = tmp;
-            }
-        }
+    for(auto i : values) {
+        std::cout << "Value: " << i << std::endl;
     }
 }
 
-void place() {
-    
+Tree * place(Tree *node, int value) {
+    if(node == NULL) {
+        node = new Tree(value);
+    } else if(node->getValue() < value) {
+        node->rightNode = place(node->rightNode, value);
+        node->rightNode->parentRoot = node;
+    } else {
+        node->leftNode = place(node->leftNode, value);
+        node->leftNode->parentRoot = node;
+    }
+
+    return node;
 }
 
 void display() {
+    Tree * dis = first;
 
+    while(dis->leftNode != NULL) {
+        dis = dis->leftNode;
+    }
+
+    std::cout << "Lowest Inputted value: " << dis->getValue() << std::endl;
 }
