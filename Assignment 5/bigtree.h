211fa49a93f5318d0c74ce2adc1631d612b37407
josmoor <josmoor@iu.edu>
2023-10-12 19:27:50 -0900

    Updated B-Tree header

--- Assignment 5/bigtree.h
+++ Assignment 5/bigtree.h
@@ -1,18 +1,29 @@
 #ifndef BIGTREE_H
 #define BIGRREE_H
 
+struct node {
+    int data;
+    int sibCount = 0;
+    node* parent = nullptr;
+    node* prev = nullptr;
+    node* next = nullptr;
+    node* children[5];
+};
+
 class BigTree {
-    struct node {
-        int data;
-        int sibCount = 1;
-        node* parent = nullptr;
-        node* prev = nullptr;
-        node* next = nullptr;
-        node* children[5];
-    }
 
     private:
-        node * root = NULL;
+        node * root = nullptr;
+
+        void setSibCount(node* where, int value) {
+            while(where->prev != nullptr)
+                where = where->prev;
+
+            do {
+                where->sibCount = value;
+                where = where->next;
+            } while(where->next != nullptr);
+        }
 
     public:
         BigTree() { }
@@ -51,7 +62,7 @@ class BigTree {
         bool hasChildren() {
             node* child = root->children[0];
 
-            if(child == NULL) return false;
+            if(child == nullptr) return false;
             else return true;
         }
 
@@ -63,21 +74,22 @@ class BigTree {
             while(look->prev != nullptr)
                 look = look->prev;
 
-            while(look != nullptr) {
+            while(look->next != nullptr) {
                 if(hasChildren(look))
                     return true;
+                look = look->next;
             }
 
             return false;
         }
 
-        int getChildrenNum() {
-            if(!hasChildren())
+        int getChildrenNum(node* look) {
+            if(!hasChildren(look))
                 return 0;
             else {
                 int ret = 1;
                 for(int i = 1; i < 4; i++)
-                    ret += root->children[i] != NULL ? 1 : 0;
+                    ret += look->children[i] != nullptr ? 1 : 0;
 
                 return ret;
             }
@@ -88,11 +100,44 @@ class BigTree {
             n->data = value;
 
             node* tmp = root;
-            bool notPlaced = true;
+            node* tmpNext = tmp;
 
-            while(notPlaced) {
-                if(!hasChildren(tmp)) {
-                    // TODO: no children in 'row' then place in correct spot in row.
+            bool isPlacable = false;
+
+            while(!isPlacable) {
+                if(!anySiblingHasChildren(tmpNext)) {
+                    tmp = tmpNext;
+
+                    do {
+                        if(value > tmpNext->data) {
+                            tmp = tmpNext;
+                            tmpNext = tmpNext->next;
+                        } else {
+                            isPlacable = true;
+
+                            if(tmpNext->prev == nullptr) {
+                                tmpNext->prev = n;
+                                n->next = tmpNext;
+                            } else {
+                                tmpNext->prev->next = n;
+                                n->prev = tmpNext->prev;
+                                n->next = tmpNext;
+                                tmpNext->prev = n;
+                            }
+
+                            break;
+                        }
+                    } while (tmpNext != nullptr);
+
+                    if(!isPlacable) { // Catcher for if the value is the greatest value
+                        tmp->next = n;
+                        n->prev = tmp;
+                        isPlacable = true;
+                    }
+
+                    setSibCount(tmpNext, tmpNext->sibCount + 1);
+                } else { // At Least one sibling has children
+                    //TODO: Compare value with each node until end is found.
                 }
             }
         }
