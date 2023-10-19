047acb9991afef116c5ab8d37964a3bf9823f859
BuildTools <unconfigured@null.spigotmc.org>
2023-10-18 22:19:05 -0400

    Completed B-Tree class

--- Assignment 5/bigtree.h
+++ Assignment 5/bigtree.h
@@ -2,12 +2,12 @@
 #define BIGRREE_H
 
 struct node {
-    int data;
-    int sibCount = 0;
+    int* data;
+    int* childCount[2];
     node* parent = nullptr;
     node* prev = nullptr;
     node* next = nullptr;
-    node* children[5];
+    node* children[2];
 };
 
 class BigTree {
@@ -15,14 +15,20 @@ class BigTree {
     private:
         node * root = nullptr;
 
-        void setSibCount(node* where, int value) {
-            while(where->prev != nullptr)
-                where = where->prev;
+        int setParentToFirst(node* tmpNextLoc) {
+            while(tmpNextLoc->prev != nullptr) {
+                tmpNextLoc = tmpNextLoc->prev;
+            }
 
-            do {
-                where->sibCount = value;
-                where = where->next;
-            } while(where->next != nullptr);
+            if(tmpNextLoc->data >= tmpNextLoc->parent->data) {
+                tmpNextLoc->parent->children[1] = tmpNextLoc;
+                tmpNextLoc->parent->childCount[1] += 1;
+                return 0;
+            } else {
+                tmpNextLoc->parent->children[0] = tmpNextLoc;
+                tmpNextLoc->parent->childCount[0] += 1;
+                return 1;
+            }
         }
 
     public:
@@ -35,27 +41,53 @@ class BigTree {
             node* tmpNext = tmp;
 
             while(root->children[0] != nullptr) {
-                if(getChildrenNum() == 0 && tmp->prev == nullptr) {
-                    tmpNext = tmp->parent;
-                    delete[] tmp->children;
-                    delete tmp;
+                if(hasChildren(tmpNext)) {
+                    tmpNext = tmpNext->children[1] != nullptr ? tmpNext->children[1] : tmpNext->children[0];
+
+                    while(tmpNext->next != nullptr)
+                        tmpNext = tmpNext->next;
+
                     tmp = tmpNext;
                 } else {
-                    int childrenNum = getChildrenNum();
-                    if(childrenNum < 1) {
-                        tmpNext = tmp->prev;
-                        delete tmp;
+                    if(tmp->prev != nullptr) {
+                        tmp = tmp->prev;
                     } else {
-                        tmpNext = tmp->children[childrenNum - 1];
-                        while(tmpNext->next != nullptr)
-                            tmpNext = tmpNext->next;
-                        tmp = tmpNext;
+                        tmp = tmp->parent;
+
+                        if(*tmp->data >= *tmp->parent->data)
+                            tmp->children[1] = nullptr;
+                        else
+                            tmp->children[0] = nullptr;
                     }
+
+                    tmpNext->data = nullptr;
+                    tmpNext->childCount[0] = nullptr;
+                    tmpNext->childCount[1] = nullptr;
+                    tmpNext->parent = nullptr;
+                    tmpNext->prev = nullptr;
+                    tmpNext->next = nullptr;
+
+                    delete[] tmpNext->children;
+                    delete[] tmpNext->childCount;
+                    delete tmpNext->parent;
+                    delete tmpNext->next;
+                    delete tmpNext->prev;
+                    delete tmpNext->data;
+                    delete tmpNext;
+
+                    tmpNext = tmp;
                 }
             }
 
-            delete tmp;
+            delete[] tmpNext->children;
+            delete[] tmpNext->childCount;
+            delete tmpNext->parent;
+            delete tmpNext->next;
+            delete tmpNext->prev;
+            delete tmpNext->data;
+
             delete tmpNext;
+            delete tmp;
             delete root;
         }
 
@@ -67,7 +99,7 @@ class BigTree {
         }
 
         bool hasChildren(node* look) {
-            return look->children[0] != nullptr;
+            return look->children[0] != nullptr || look->children[1] != nullptr;
         }
 
         bool anySiblingHasChildren(node* look) {
@@ -95,50 +127,124 @@ class BigTree {
             }
         }
 
-        void insert(int value) {
-            node* n = new node;
-            n->data = value;
+        void insert(node* newNode, node* tmpRoot) {
+            node* tmpNext = tmpRoot;
+            bool isPlaced = false;
 
-            node* tmp = root;
-            node* tmpNext = tmp;
+            if(root == nullptr) {
+                root = newNode;
+                return;
+            }
 
-            bool isPlacable = false;
-
-            while(!isPlacable) {
-                if(!anySiblingHasChildren(tmpNext)) {
-                    tmp = tmpNext;
-
-                    do {
-                        if(value > tmpNext->data) {
-                            tmp = tmpNext;
-                            tmpNext = tmpNext->next;
-                        } else {
-                            isPlacable = true;
-
-                            if(tmpNext->prev == nullptr) {
-                                tmpNext->prev = n;
-                                n->next = tmpNext;
-                            } else {
-                                tmpNext->prev->next = n;
-                                n->prev = tmpNext->prev;
-                                n->next = tmpNext;
-                                tmpNext->prev = n;
-                            }
-
-                            break;
-                        }
-                    } while (tmpNext != nullptr);
-
-                    if(!isPlacable) { // Catcher for if the value is the greatest value
-                        tmp->next = n;
-                        n->prev = tmp;
-                        isPlacable = true;
-                    }
-
-                    setSibCount(tmpNext, tmpNext->sibCount + 1);
-                } else { // At Least one sibling has children
-                    //TODO: Compare value with each node until end is found.
+            if(!anySiblingHasChildren(tmpNext)) {
+                while(tmpNext->next != nullptr) {
+                    if(tmpNext->data > newNode->data)
+                        break;
+                    else
+                        tmpNext = tmpNext->next;
                 }
+
+                if(tmpNext->data > newNode->data) {
+                    if(tmpNext->prev == nullptr) {
+                        tmpNext->prev = newNode;
+                        newNode->next = tmpNext;
+                    } else {
+                        tmpNext->prev->next = newNode;
+                        newNode->prev = tmpNext->prev;
+                        newNode->next = tmpNext;
+                        tmpNext->prev = newNode;
+                    }
+                } else {
+                    if(tmpNext->next == nullptr) {
+                        tmpNext->next = newNode;
+                        newNode->prev = tmpNext;
+                    }  else {
+                        tmpNext->next->prev = newNode;
+                        newNode->next = tmpNext->next;
+                        newNode->prev = tmpNext;
+                        tmpNext->next = newNode;
+                    }
+                }
+
+                newNode->parent = tmpNext->parent;
+
+                int which = setParentToFirst(tmpNext);
+                reconstruct(tmpNext, which);
+            } else { // Has Children
+                bool hasBeenFound = false;
+
+                while(tmpNext->next != nullptr) {
+                    if(tmpNext->data > newNode->data) {
+                        tmpRoot = tmpNext->children[0];
+                        insert(newNode, tmpNext);
+                        hasBeenFound = true;
+                        break;
+                    } else
+                        tmpNext = tmpNext->next;
+                }
+
+                if(!hasBeenFound) { // Compare last node
+                    if(tmpNext->data < newNode->data) {
+                        tmpRoot = tmpNext->children[0];
+                        insert(newNode, tmpRoot);
+                    } else {
+                        tmpRoot = tmpNext->children[1];
+                        insert(newNode, tmpRoot);
+                    }
+                }
+            }
+        }
+
+        void reconstruct(node* tmpRoot, int childID) {
+            if(*tmpRoot->parent->childCount[childID] != 5)
+                return;
+            else {
+                while(tmpRoot->prev != nullptr)
+                    tmpRoot = tmpRoot->prev;
+
+                tmpRoot = tmpRoot->next->next; // Gets middle (medium) value of all siblings
+
+                tmpRoot->prev->prev->parent = tmpRoot;
+                tmpRoot->prev->parent = tmpRoot;        // Set previous two values to children of current node
+
+                if(tmpRoot->data < tmpRoot->parent->data) {
+                    tmpRoot->parent->children[0] = tmpRoot->next; // Set parent child to next node
+                    tmpRoot->childCount[0] = new int(2);
+                    tmpRoot->next->childCount[0] = new int(2);
+                    tmpRoot->prev->next = nullptr;
+                    tmpRoot->next->prev = nullptr;
+
+                    if(tmpRoot->parent->prev != nullptr) { // Parent has LEFT sib
+                        tmpRoot->parent->prev->next = tmpRoot;
+                        tmpRoot->prev = tmpRoot->parent->prev;
+                        tmpRoot->next = tmpRoot->parent;
+                        tmpRoot->parent->prev = tmpRoot;
+                        tmpRoot->parent = tmpRoot->next->parent;
+                    } else { // Parent has NO LEFT sib
+                        tmpRoot->prev = nullptr;
+                        tmpRoot->parent->prev = tmpRoot;
+                        tmpRoot->next = tmpRoot->parent;
+                        tmpRoot->parent = tmpRoot->next->parent;
+                    }
+                } else { // >=
+                    tmpRoot->parent->children[1] = nullptr;
+                    tmpRoot->parent->childCount[1] = 0;
+                    tmpRoot->childCount[0] = new int(2);
+                    tmpRoot->childCount[1] = new int(2);
+                    tmpRoot->children[0] = tmpRoot->prev->prev;
+                    tmpRoot->children[1] = tmpRoot->next->next;
+                    tmpRoot->prev->next = nullptr;
+                    tmpRoot->next->prev = nullptr;
+                    tmpRoot->next->next->parent = tmpRoot;
+                    tmpRoot->next->parent = tmpRoot;
+                    tmpRoot->parent->next = tmpRoot;
+                    tmpRoot->next = nullptr;
+                    tmpRoot->prev = tmpRoot->parent;
+                    tmpRoot->parent = tmpRoot->prev->parent;
+                }
+
+                int which = setParentToFirst(tmpRoot);
+                reconstruct(tmpRoot, which);
             }
         }
 
